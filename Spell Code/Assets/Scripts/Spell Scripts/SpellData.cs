21eed53a973e10dd50b2e26752316f6d49b11ee2
Reaper777 <jayeshsingh62@gmail.com>
2025-09-17 20:29:54 -0400

    created a basic scriptable object for a basic spell and created a basic spell out of it

---
+++ Spell Code/Assets/Scripts/Spell Scripts/SpellData.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public enum SpellDirection 
+{ 
+    None, 
+    Up, 
+    Down, 
+    Left, 
+    Right 
+}
+
+public enum SpellType
+{
+    Passive,
+    Active
+}
+
+
+[CreateAssetMenu(fileName = "New Spell", menuName = "Spells/Spell Data")]
+public class SpellData : ScriptableObject
+{
+    [Header("Identification & Network")]
+    public string spellName;
+    public ushort spellId; 
+
+    [Header("Casting Requirements")]
+    public SpellDirection[] inputSequence;
+    public float cooldown;
+}

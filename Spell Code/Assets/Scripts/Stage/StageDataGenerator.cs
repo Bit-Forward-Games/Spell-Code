8831da542b7b021dcbe151393a1e08ac6676f990
Otiong <oscarloltiong@gmail.com>
2025-10-04 16:54:58 -0400

    Added new script

---
+++ Spell Code/Assets/Scripts/Stage/StageDataGenerator.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class StageDataGenerator : MonoBehaviour
+{
+    // Have a struct for the platforms and solids
+    // List platforms
+    public List<Bounds> solids;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    void 
+    */
+}

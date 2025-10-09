7ba9d3588d459468ede768630c39c732bcbf26fd
JAYESH SINGH <jayeshsingh62@gmail.com>
2025-10-09 13:39:44 -0400

    added the active spell 4

---
+++ Spell Code/Assets/Scripts/Projectiles/Active_Spell_4_prj.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Active_Spell_4_prj : BaseProjectile
+{
+    public Active_Spell_4_prj()
+    {
+        projName = "Active_Spell_4";
+        //hSpeed = 3f;
+        //vSpeed = 0f;
+        lifeSpan = 30; // lasts for 300 logic frames
+
+        animFrames = new AnimFrames(new List<int>(), new List<int>() { 2, 2, 2, 2, 2, 2 }, false);
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public override void SpawnProjectile(bool facingRight, Vector2 spawnOffset)
+    {
+        base.SpawnProjectile(facingRight, spawnOffset);
+        activeHitboxGroupIndex = 0;
+        this.hSpeed = (facingRight ? 1 : -1) * 8;
+    }
+
+    public override void LoadProjectile()
+    {
+        base.LoadProjectile();
+        projectileHitboxes = new HitboxGroup[2];
+        projectileHitboxes[1] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>
+            {
+                new HitboxData
+                {
+                    xOffset = 0,
+                    yOffset = 3,
+                    width = 160,
+                    height = 6,
+                    xKnockback = 0,
+                    yKnockback = 1,
+                    damage = 10,
+                    hitstun = 30,
+                    attackLvl = 2,
+                    cancelOptions = new List<int> { } // No cancel options
+                }
+            },
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+        projectileHitboxes[0] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>(),
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+    }
+
+    public override void ProjectileUpdate()
+    {
+        base.ProjectileUpdate();
+        //okay so this logic is a bit wonky to understand but basically if the ball hits something,
+        //it switches to the non-hitting hitbox group, sets its horizontal speed to 0,
+        //and then waits until the animation is done to delete itself.
+        if (logicFrame >= animFrames.frameLengths.Take(1).Sum() + 1)
+        {
+            activeHitboxGroupIndex = 1;
+        }
+        if (logicFrame >= animFrames.frameLengths.Sum())
+        {
+            ProjectileManager.Instance.DeleteProjectile(this);
+        }
+        //this basically checks if the projectile hit something
+
+    }
+}

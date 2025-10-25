63bdf240eb789d79cb24b76e0da68257ec631e6d
Otiong <oscarloltiong@gmail.com>
2025-10-24 23:57:39 -0400

    Automatic SO generator
    Made new scriptable objects generate for each scene automatically. These SOs are named based on on the scene name automatically

--- Spell Code/Assets/Scripts/Stage/StageDataBuilder.cs
+++ Spell Code/Assets/Scripts/Stage/StageDataBuilder.cs
@@ -1,4 +1,8 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class StageDataBuilder : MonoBehaviour
 {
@@ -12,6 +16,8 @@ public class StageDataBuilder : MonoBehaviour
         platforms = GameObject.FindGameObjectsWithTag("Platform");
         solids = GameObject.FindGameObjectsWithTag("Solid");
 
+        stageDataSO = ScriptableObject.CreateInstance<StageDataSO>();
+
         stageDataSO.platformCenter = new Vector2[platforms.Length];
         stageDataSO.platformExtent = new Vector2[platforms.Length];
 
@@ -19,6 +25,8 @@ public class StageDataBuilder : MonoBehaviour
         stageDataSO.solidExtent = new Vector2[solids.Length];
 
         GetStageData();
+
+        SaveStageData();
     }
 
     void GetStageData()
@@ -41,4 +49,31 @@ public class StageDataBuilder : MonoBehaviour
             j++;
         }
     }
+
+    #if UNITY_EDITOR
+    void SaveStageData()
+    {
+        // Specify the path where you want to save it
+        // Must start with "Assets/" and include file extension
+        string directory = "Assets/SO";
+        
+        // Make sure the directory exists
+        if (!System.IO.Directory.Exists(directory))
+        {
+            System.IO.Directory.CreateDirectory(directory);
+        }
+
+        string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
+    
+        // Create the path with scene name + "StageDataSO"
+        string path = $"{directory}/{sceneName}StageDataSO.asset";
+        
+        // Create the asset at the specified path
+        AssetDatabase.CreateAsset(stageDataSO, path);
+        
+        // Save changes
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+    }
+    #endif
 }

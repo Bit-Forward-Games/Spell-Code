ef3e65ce52b14f1aaa7f418dd1c389cd41800ca8
Billy Guzik <guzikbilly@gmail.com>
2025-11-04 10:43:06 -0500

    yes

--- Spell Code/Assets/Scripts/Stage/StageDataBuilder.cs
+++ Spell Code/Assets/Scripts/Stage/StageDataBuilder.cs
@@ -35,7 +35,9 @@ public class StageDataBuilder : MonoBehaviour
         if (Input.GetKey("s") && Input.GetKeyDown("o"))
         {
             GetStageData();
+            #if UNITY_EDITOR
             SaveStageData();
+            #endif
         }
     }
 

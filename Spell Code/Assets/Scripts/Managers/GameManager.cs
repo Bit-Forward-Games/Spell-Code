fcb2cd483b47796ab9b24ab1852c8d031a318b3c
Billy Guzik <guzikbilly@gmail.com>
2025-11-18 10:48:28 -0500

    Revert "player onboarding main menu"
    This reverts commit 42ba3538d3c288c22651613a755dd1742d805b13.

--- Spell Code/Assets/Scripts/Managers/GameManager.cs
+++ Spell Code/Assets/Scripts/Managers/GameManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using TMPro;
-using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour/*NonPersistantSingleton<GameManager>*/
 {
@@ -46,26 +45,8 @@ public class GameManager : MonoBehaviour/*NonPersistantSingleton<GameManager>*/
     public int roundEndTransitionTime = 2;
     public TextMeshProUGUI playerWinText;
 
-    //main menu stuff (we will likely remove all of this later, its just a rehash of shop manager stuff)
+    //main menu stuff
     public bool playersChosenSpell;
-    public Image p1_spellCard;
-    public Image p2_spellCard;
-    public Image p3_spellCard;
-    public Image p4_spellCard;
-
-    [SerializeField]
-    private List<string> p1_choices;
-    [SerializeField]
-    private List<string> p2_choices;
-    [SerializeField]
-    private List<string> p3_choices;
-    [SerializeField]
-    private List<string> p4_choices;
-
-    private int p1_index = 0;
-    private int p2_index = 0;
-    private int p3_index = 0;
-    private int p4_index = 0;
 
     private void Awake()
     {
@@ -89,10 +70,6 @@ public class GameManager : MonoBehaviour/*NonPersistantSingleton<GameManager>*/
         isSaved = false;
 
         playerWinText.enabled = false;
-        p1_spellCard.enabled = false;
-        p2_spellCard.enabled = false;
-        p3_spellCard.enabled = false;
-        p4_spellCard.enabled = false;
 
         dataManager = DataManager.Instance;
 
@@ -218,159 +195,6 @@ public class GameManager : MonoBehaviour/*NonPersistantSingleton<GameManager>*/
 
         if(activeScene.name == "MainMenu")
         {
-            //player 1 stuff
-            if (players[0] != null)
-            {
-                if (players[0].chosenStartingSpell == false && players[0].isSpawned)
-                {
-                    //cycle spells (spellWeave button)
-                    if (players[0].input.ButtonStates[0] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p1 pressed cycle spell");
-                        if (p1_index == 1)
-                        {
-                            p1_index = 0;
-                        }
-                        else
-                        {
-                            p1_index++;
-                        }
-
-                        p1_spellCard.sprite = SpellDictionary.Instance.spellDict[p1_choices[p1_index]].shopSprite;
-                    }
-
-                    //choose spell (jump button)
-                    if (players[0].input.ButtonStates[1] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p1 chose a spell");
-                        players[0].AddSpellToSpellList(p1_choices[p1_index]);
-                        players[0].chosenStartingSpell = true;
-                        p1_spellCard.enabled = false;
-                    }
-                }
-
-                if (players[0].isSpawned == false)
-                {
-                    GenerateStartingSpells(0);
-                    p1_spellCard.enabled = true;
-                    players[0].isSpawned = true;
-                }
-            }
-            //player 2 stuff
-            if (players[1] != null)
-            {
-                if (players[1].chosenStartingSpell == false && players[1].isSpawned)
-                {
-                    //cycle spells (spellWeave button)
-                    if (players[1].input.ButtonStates[0] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p2 pressed cycle spell");
-                        if (p2_index == 1)
-                        {
-                            p2_index = 0;
-                        }
-                        else
-                        {
-                            p2_index++;
-                        }
-
-                        p2_spellCard.sprite = SpellDictionary.Instance.spellDict[p2_choices[p2_index]].shopSprite;
-                    }
-
-                    //choose spell (jump button)
-                    if (players[1].input.ButtonStates[1] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p2 chose a spell");
-                        players[1].AddSpellToSpellList(p2_choices[p2_index]);
-                        players[1].chosenStartingSpell = true;
-                        p2_spellCard.enabled = false;
-                    }
-                }
-
-                if (players[1].isSpawned == false)
-                {
-                    GenerateStartingSpells(1);
-                    p2_spellCard.enabled = true;
-                    players[1].isSpawned = true;
-                }
-            }
-            //player 3 stuff
-            if (players[2] != null)
-            {
-                if (players[2].chosenStartingSpell == false && players[2].isSpawned)
-                {
-                    //cycle spells (spellWeave button)
-                    if (players[2].input.ButtonStates[2] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p3 pressed cycle spell");
-                        if (p3_index == 1)
-                        {
-                            p3_index = 0;
-                        }
-                        else
-                        {
-                            p3_index++;
-                        }
-
-                        p3_spellCard.sprite = SpellDictionary.Instance.spellDict[p3_choices[p3_index]].shopSprite;
-                    }
-
-                    //choose spell (jump button)
-                    if (players[2].input.ButtonStates[1] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p3 chose a spell");
-                        players[2].AddSpellToSpellList(p3_choices[p3_index]);
-                        players[2].chosenStartingSpell = true;
-                        p3_spellCard.enabled = false;
-                    }
-                }
-
-                if (players[2].isSpawned == false)
-                {
-                    GenerateStartingSpells(2);
-                    p3_spellCard.enabled = true;
-                    players[2].isSpawned = true;
-                }
-            }
-            //player 4 stuff
-            if (players[3] != null)
-            {
-                if (players[3].chosenStartingSpell == false && players[3].isSpawned)
-                {
-                    //cycle spells (spellWeave button)
-                    if (players[3].input.ButtonStates[0] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p4 pressed cycle spell");
-                        if (p4_index == 1)
-                        {
-                            p4_index = 0;
-                        }
-                        else
-                        {
-                            p4_index++;
-                        }
-
-                        p4_spellCard.sprite = SpellDictionary.Instance.spellDict[p4_choices[p4_index]].shopSprite;
-                    }
-
-                    //choose spell (jump button)
-                    if (players[3].input.ButtonStates[1] == ButtonState.Pressed)
-                    {
-                        Debug.Log("p4 chose a spell");
-                        players[3].AddSpellToSpellList(p4_choices[p4_index]);
-                        players[3].chosenStartingSpell = true;
-                        p4_spellCard.enabled = false;
-                    }
-                }
-
-                if (players[3].isSpawned == false)
-                {
-                    GenerateStartingSpells(0);
-                    p4_spellCard.enabled = true;
-                    players[3].isSpawned = true;
-                }
-            }
-
             goDoorPrefab.CheckOpenDoor();
             
             if(goDoorPrefab.CheckAllPlayersReady())
@@ -632,35 +456,5 @@ public class GameManager : MonoBehaviour/*NonPersistantSingleton<GameManager>*/
         {
             MainMenuScreen.SetActive(isActive);
         }
-    }
-
-    public void GenerateStartingSpells(int index)
-    {
-
-        if (index == 0)
-        {
-            p1_choices = new List<string>();
-            p1_choices.Add("SkillshotSlash");
-            p1_choices.Add("MightOfZeus");
-        }
-        if (index == 1)
-        {
-            p2_choices = new List<string>();
-            p2_choices.Add("SkillshotSlash");
-            p2_choices.Add("MightOfZeus");
-        }
-        if (index == 2)
-        {
-            p3_choices = new List<string>();
-            p3_choices.Add("SkillshotSlash");
-            p3_choices.Add("MightOfZeus");
-        }
-        if (index == 3)
-        {
-            p4_choices = new List<string>();
-            p4_choices.Add("SkillshotSlash");
-            p4_choices.Add("MightOfZeus");
-        }
-
     }
 }

6762206c8900c9592d74a56c48c5c41357274b84
Billy Guzik <guzikbilly@gmail.com>
2026-01-20 10:38:26 -0500

    floppy anim good

--- Spell Code/Assets/Scripts/SpellCode_FloppyDisk.cs
+++ Spell Code/Assets/Scripts/SpellCode_FloppyDisk.cs
@@ -1,13 +1,23 @@
+using NUnit.Framework;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Windows;
 using System.IO;
 
 public class SpellCode_FloppyDisk : MonoBehaviour
 {
     public Animator diskAnimator;
     Bounds diskBounds;
+    public string diskName;
+    public Image shopSprite;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()

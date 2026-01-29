938955526103fd713a8ed17aba9850d5101095ea
Otiong <oscarloltiong@gmail.com>
2026-01-29 15:46:34 -0500

    Empty text and cooldown bar disabled when player doesn't have the spell

--- Spell Code/Assets/Scripts/UI/TempSpellDisplay.cs
+++ Spell Code/Assets/Scripts/UI/TempSpellDisplay.cs
@@ -2,6 +2,7 @@ using TMPro;
 using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Linq;
 
 public class TempSpellDisplay : MonoBehaviour
 {
@@ -14,12 +15,6 @@ public class TempSpellDisplay : MonoBehaviour
 
     public void Start()
     {
-        // arrowsSprite[0] = Resources.Load<Sprite>("Arrows/Up_Arrow");
-        // arrowsSprite[1] = Resources.Load<Sprite>("Arrows/Down_Arrow");
-        // arrowsSprite[2] = Resources.Load<Sprite>("Arrows/Right_Arrow");
-        // arrowsSprite[3] = Resources.Load<Sprite>("Arrows/Left_Arrow");
-        // arrowsSprite[3] = Resources.Load<Sprite>("Sprites/Test sprites/UI Elements/Player Panel/Arrows/Left_Arrow");
-
     }
 
     public void UpdateSpellDisplay(int playerIndex, bool showInputs = false)
@@ -29,8 +24,17 @@ public class TempSpellDisplay : MonoBehaviour
 
         for (int i = 0; i < spellSlots.Count; i++)
         {
+            GameObject parent = FindParentByNameContains(cooldownFills[i].transform, "CooldownBar");
+
+            if (parent == null)
+            {
+                Debug.LogWarning($"CooldownBar parent not found for cooldownFills[{i}] ({cooldownFills[i].name})");
+                continue; // Skip this iteration
+            }
+            
             if (i < playerSpells.Count)
             {
+                parent.gameObject.SetActive(true);
                 if (showInputs)
                 {
                     spellSlots[i].text = PlayerController.ConvertCodeToString(playerSpells[i].spellInput);
@@ -39,11 +43,11 @@ public class TempSpellDisplay : MonoBehaviour
                 {
                     spellSlots[i].text = playerSpells[i].spellName;
                 }
-
             }
             else
             {
-                spellSlots[i].text = "Empty";
+                parent.gameObject.SetActive(false);
+                spellSlots[i].text = "";
             }
             spellSlots[i].alignment = invertAlign ? TextAlignmentOptions.Right : TextAlignmentOptions.Left;
         }
@@ -69,6 +73,12 @@ public class TempSpellDisplay : MonoBehaviour
         }
     }
 
+    GameObject FindParentByNameContains(Transform childTransform, string nameToContain)
+    {
+        return childTransform.GetComponentsInParent<Transform>()
+            .FirstOrDefault(t => t.name.Contains(nameToContain))?.gameObject;
+    }
+
     public void OldUpdateSpellDisplay(int playerIndex)
     {
         var playerSpells = GameManager.Instance.players[playerIndex].spellList;
@@ -80,7 +90,7 @@ public class TempSpellDisplay : MonoBehaviour
             }
             else
             {
-                spellSlots[i].text = "Empty";
+                spellSlots[i].text = "";
             }
             spellSlots[i].alignment = invertAlign ? TextAlignmentOptions.Right : TextAlignmentOptions.Left;
         }

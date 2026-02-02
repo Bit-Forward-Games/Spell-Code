9d107aaf1d9caab98e837053156834f83cc44a49
Billy Guzik <guzikbilly@gmail.com>
2026-02-02 13:27:50 -0500

    should no longer be able to overflow the spell list for players

--- Spell Code/Assets/Scripts/Managers/ShopManager.cs
+++ Spell Code/Assets/Scripts/Managers/ShopManager.cs
@@ -48,6 +48,8 @@ public class ShopManager : MonoBehaviour
     public TextMeshProUGUI p3_spellText;
     public TextMeshProUGUI p4_spellText;
 
+    public bool canShop = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -63,6 +65,14 @@ public class ShopManager : MonoBehaviour
         if (gameManager.players[3] == null)
         {
             p4_spellCard.enabled = false;
+        }
+
+        if (gameManager.players[0].spellList.Count >= 6)
+        {
+            {
+                canShop = false;
+                StartCoroutine(Shop());
+            }
         }
 
         GenerateSpellChoices();
@@ -90,172 +100,175 @@ public class ShopManager : MonoBehaviour
 
     public void ShopUpdate(ulong[] playerInputs)
     {
-        for (int i = 0; i < playerInputs.Length; i++)
+        if (canShop == true)
         {
-            inputSnapshots[i] = InputConverter.ConvertFromLong(playerInputs[i]);
-        }
-        
-        //player 1 stuff
-        if (gameManager.players[0].chosenSpell == false)
-        {
-            //cycle spells (spellWeave button)
-            if (inputSnapshots[0].ButtonStates[0] == ButtonState.Pressed)
+            for (int i = 0; i < playerInputs.Length; i++)
             {
-                Debug.Log("p1 pressed cycle spell");
-                if (p1_index == 2)
-                {
-                    p1_index = 0;
-                }
-                else
-                {
-                    p1_index++;
-                }
-
-                p1_spellCard.sprite = SpellDictionary.Instance.spellDict[p1_choices[p1_index]].shopSprite;
+                inputSnapshots[i] = InputConverter.ConvertFromLong(playerInputs[i]);
             }
 
-            //choose spell (jump button)
-            if (inputSnapshots[0].ButtonStates[1] == ButtonState.Pressed)
-            {
-                Debug.Log("p1 chose a spell");
-                GivePlayerSpell(0, p1_choices[p1_index]);
-                gameManager.players[0].chosenSpell = true;
-                p1_spellCard.enabled = false;
-                inputSnapshots[0].SetNull();
-            }
-        }
-
-        //player 2 stuff
-        if (gameManager.players[1].chosenSpell == false)
-        {
-            //cycle spells (spellWeave button)
-            if (inputSnapshots[1].ButtonStates[0] == ButtonState.Pressed)
-            {
-                Debug.Log("p2 pressed cycle spell");
-                if (p2_index == 2)
-                {
-                    p2_index = 0;
-                }
-                else
-                {
-                    p2_index++;
-                }
-
-                p2_spellCard.sprite = SpellDictionary.Instance.spellDict[p2_choices[p2_index]].shopSprite;
-            }
-
-            //choose spell (jump button)
-            if (inputSnapshots[1].ButtonStates[1] == ButtonState.Pressed)
-            {
-                Debug.Log("p2 chose a spell");
-                GivePlayerSpell(1, p2_choices[p2_index]);
-                gameManager.players[1].chosenSpell = true;
-                p2_spellCard.enabled = false;
-                inputSnapshots[1].SetNull();
-            }
-        }
-
-        //if player 3 is not null
-        if (gameManager.players[2] != null)
-        {
-            //player 3 stuff
-            if (gameManager.players[2].chosenSpell == false)
+            //player 1 stuff
+            if (gameManager.players[0].chosenSpell == false)
             {
                 //cycle spells (spellWeave button)
-                if (inputSnapshots[2].ButtonStates[0] == ButtonState.Pressed)
+                if (inputSnapshots[0].ButtonStates[0] == ButtonState.Pressed)
                 {
-                    Debug.Log("p3 pressed cycle spell");
-                    if (p3_index == 2)
+                    Debug.Log("p1 pressed cycle spell");
+                    if (p1_index == 2)
                     {
-                        p3_index = 0;
+                        p1_index = 0;
                     }
                     else
                     {
-                        p3_index++;
+                        p1_index++;
                     }
 
-                    p3_spellCard.sprite = SpellDictionary.Instance.spellDict[p3_choices[p3_index]].shopSprite;
+                    p1_spellCard.sprite = SpellDictionary.Instance.spellDict[p1_choices[p1_index]].shopSprite;
                 }
 
                 //choose spell (jump button)
-                if (inputSnapshots[2].ButtonStates[1] == ButtonState.Pressed)
+                if (inputSnapshots[0].ButtonStates[1] == ButtonState.Pressed)
                 {
-                    Debug.Log("p3 chose a spell");
-                    GivePlayerSpell(2, p3_choices[p3_index]);
-                    gameManager.players[2].chosenSpell = true;
-                    p3_spellCard.enabled = false;
-                    inputSnapshots[2].SetNull();
+                    Debug.Log("p1 chose a spell");
+                    GivePlayerSpell(0, p1_choices[p1_index]);
+                    gameManager.players[0].chosenSpell = true;
+                    p1_spellCard.enabled = false;
+                    inputSnapshots[0].SetNull();
                 }
             }
-        }
 
-        //if player 4 is not null
-        if (gameManager.players[3] != null)
-        {
-            //player 4 stuff
-            if (gameManager.players[3].chosenSpell == false)
+            //player 2 stuff
+            if (gameManager.players[1].chosenSpell == false)
             {
                 //cycle spells (spellWeave button)
-                if (inputSnapshots[3].ButtonStates[0] == ButtonState.Pressed)
+                if (inputSnapshots[1].ButtonStates[0] == ButtonState.Pressed)
                 {
-                    Debug.Log("p4 pressed cycle spell");
-                    if (p4_index == 2)
+                    Debug.Log("p2 pressed cycle spell");
+                    if (p2_index == 2)
                     {
-                        p4_index = 0;
+                        p2_index = 0;
                     }
                     else
                     {
-                        p4_index++;
+                        p2_index++;
                     }
 
-                    p4_spellCard.sprite = SpellDictionary.Instance.spellDict[p4_choices[p4_index]].shopSprite;
+                    p2_spellCard.sprite = SpellDictionary.Instance.spellDict[p2_choices[p2_index]].shopSprite;
                 }
 
                 //choose spell (jump button)
-                if (inputSnapshots[3].ButtonStates[1] == ButtonState.Pressed)
+                if (inputSnapshots[1].ButtonStates[1] == ButtonState.Pressed)
                 {
-                    Debug.Log("p4 chose a spell");
-                    GivePlayerSpell(3, p4_choices[p4_index]);
-                    gameManager.players[3].chosenSpell = true;
-                    p4_spellCard.enabled = false;
-                    inputSnapshots[3].SetNull();
-                }
-            }
-        }
-
-        //escape logic
-        int playersChosen = 0;
-        if (!allPlayersChosen)
-        {
-            foreach (PlayerController player in gameManager.players)
-            {
-                if (player != null)
-                {
-                    if (player.chosenSpell) { playersChosen++; }
+                    Debug.Log("p2 chose a spell");
+                    GivePlayerSpell(1, p2_choices[p2_index]);
+                    gameManager.players[1].chosenSpell = true;
+                    p2_spellCard.enabled = false;
+                    inputSnapshots[1].SetNull();
                 }
             }
 
-            if (playersChosen == gameManager.playerCount)
+            //if player 3 is not null
+            if (gameManager.players[2] != null)
             {
-                allPlayersChosen = true;
-            }
-            else
-            {
-                playersChosen = 0;
-            }
-        }
-        if (allPlayersChosen && !backToGameplay)
-        {
-            foreach (PlayerController player in gameManager.players)
-            {
-                if (player != null)
+                //player 3 stuff
+                if (gameManager.players[2].chosenSpell == false)
                 {
-                    player.playerNum.enabled = true;
-                    player.inputDisplay.enabled = true;
+                    //cycle spells (spellWeave button)
+                    if (inputSnapshots[2].ButtonStates[0] == ButtonState.Pressed)
+                    {
+                        Debug.Log("p3 pressed cycle spell");
+                        if (p3_index == 2)
+                        {
+                            p3_index = 0;
+                        }
+                        else
+                        {
+                            p3_index++;
+                        }
+
+                        p3_spellCard.sprite = SpellDictionary.Instance.spellDict[p3_choices[p3_index]].shopSprite;
+                    }
+
+                    //choose spell (jump button)
+                    if (inputSnapshots[2].ButtonStates[1] == ButtonState.Pressed)
+                    {
+                        Debug.Log("p3 chose a spell");
+                        GivePlayerSpell(2, p3_choices[p3_index]);
+                        gameManager.players[2].chosenSpell = true;
+                        p3_spellCard.enabled = false;
+                        inputSnapshots[2].SetNull();
+                    }
                 }
             }
-            backToGameplay = true;
-            StartCoroutine(Shop());
+
+            //if player 4 is not null
+            if (gameManager.players[3] != null)
+            {
+                //player 4 stuff
+                if (gameManager.players[3].chosenSpell == false)
+                {
+                    //cycle spells (spellWeave button)
+                    if (inputSnapshots[3].ButtonStates[0] == ButtonState.Pressed)
+                    {
+                        Debug.Log("p4 pressed cycle spell");
+                        if (p4_index == 2)
+                        {
+                            p4_index = 0;
+                        }
+                        else
+                        {
+                            p4_index++;
+                        }
+
+                        p4_spellCard.sprite = SpellDictionary.Instance.spellDict[p4_choices[p4_index]].shopSprite;
+                    }
+
+                    //choose spell (jump button)
+                    if (inputSnapshots[3].ButtonStates[1] == ButtonState.Pressed)
+                    {
+                        Debug.Log("p4 chose a spell");
+                        GivePlayerSpell(3, p4_choices[p4_index]);
+                        gameManager.players[3].chosenSpell = true;
+                        p4_spellCard.enabled = false;
+                        inputSnapshots[3].SetNull();
+                    }
+                }
+            }
+
+            //escape logic
+            int playersChosen = 0;
+            if (!allPlayersChosen)
+            {
+                foreach (PlayerController player in gameManager.players)
+                {
+                    if (player != null)
+                    {
+                        if (player.chosenSpell) { playersChosen++; }
+                    }
+                }
+
+                if (playersChosen == gameManager.playerCount)
+                {
+                    allPlayersChosen = true;
+                }
+                else
+                {
+                    playersChosen = 0;
+                }
+            }
+            if (allPlayersChosen && !backToGameplay)
+            {
+                foreach (PlayerController player in gameManager.players)
+                {
+                    if (player != null)
+                    {
+                        player.playerNum.enabled = true;
+                        player.inputDisplay.enabled = true;
+                    }
+                }
+                backToGameplay = true;
+                StartCoroutine(Shop());
+            }
         }
     }
 

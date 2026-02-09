d2c16414e40a7c3c1ee46202bf96efb9a2c2df79
FMPO <ple5024@rit.edu>
2026-02-08 22:29:19 -0500

    Added third BigStox spell: Coin Toss

---
+++ Spell Code/Assets/Scripts/Projectiles/CoinTossHeads_prj.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using BestoNet.Types;
+
+using Fixed = BestoNet.Types.Fixed32;
+using FixedVec2 = BestoNet.Types.Vector2<BestoNet.Types.Fixed32>;
+
+public class CoinTossHeads_prj : BaseProjectile
+{
+    
+    public CoinTossHeads_prj()
+    {
+        projName = "CoinTossHeads";
+        hSpeed = Fixed.FromInt(1);
+        vSpeed = Fixed.FromInt(0);
+        lifeSpan = 600; // lasts for 120 logic frames
+        deleteOnHit = false;
+        animFrames = new AnimFrames(new List<int>(), new List<int>() { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4 }, false);
+    }
+    
+    public override void SpawnProjectile(bool facingRight, FixedVec2 spawnOffset)
+    {
+        base.SpawnProjectile(facingRight, spawnOffset);
+        activeHitboxGroupIndex = 0;
+        hSpeed = Fixed.FromInt((facingRight ? 1 : -1) * 4); 
+        vSpeed = Fixed.FromInt(10); 
+    }
+    public override void LoadProjectile()
+    {
+
+        deleteOnHit = false;
+        projectileHitboxes = new HitboxGroup[2];
+        projectileHitboxes[0] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>
+            {
+                new HitboxData
+                {
+                    xOffset = -20,
+                    yOffset = 20,
+                    width = 40,
+                    height = 40,
+                    xKnockback = 3,
+                    yKnockback = 10,
+                    damage = 20,
+                    hitstun = 45,
+                    attackLvl = 4,
+                }
+            },
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+        projectileHitboxes[1] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>(),
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+        base.LoadProjectile();
+    }
+
+    public override void ProjectileUpdate()
+    {
+        base.ProjectileUpdate();
+        
+        //okay so this logic is a bit wonky to understand but basically if the ball hits something,
+        //it switches to the non-hitting hitbox group, sets its horizontal speed to 0,
+        //and then waits until the animation is done to delete itself.
+        if (logicFrame == animFrames.frameLengths.Take(16).Sum() || logicFrame >= animFrames.frameLengths.Sum())
+        {
+            ProjectileManager.Instance.DeleteProjectile(this);
+        }
+        //this basically checks if the projectile hit something
+        if (playerIgnoreArr.Any(ignore => ignore))
+        {
+            hSpeed = Fixed.FromInt(0);
+            vSpeed = Fixed.FromInt(0);
+            activeHitboxGroupIndex = 1;
+
+            playerIgnoreArr = new bool[4] { false, false, false, false };
+            logicFrame = animFrames.frameLengths.Take(16).Sum()+1; //set the logic frame to the start of the end animation
+        }
+        else if(logicFrame <= animFrames.frameLengths.Take(16).Sum())
+        {
+            vSpeed -= owner.gravity; // Apply gravity to the vertical speed
+        }
+    }
+}
---
+++ Spell Code/Assets/Scripts/Spell Scripts/Active Spells/CoinToss.cs
@@ -0,0 +1,69 @@
+using UnityEngine;
+
+
+using Fixed = BestoNet.Types.Fixed32;
+using FixedVec2 = BestoNet.Types.Vector2<BestoNet.Types.Fixed32>;
+
+public class CoinToss : SpellData
+{
+    public bool doesCrit = false;
+    public CoinToss()
+    {
+        spellName = "CoinToss";
+        brands = new Brand[] { Brand.BigStox };
+        cooldown = 240;
+        spellInput = 0b_0000_0000_0000_0000_0000_1101_0000_0010; // Example input sequence
+        spellType = SpellType.Active;
+        procConditions = new ProcCondition[1] {ProcCondition.ActiveOnCast};
+        description = "Toss a coin, dealing massive damage if it lands on heads. There is a chance equal to your \"Stock Stability\" that you throw ad Loaded coin. You gain 15% \"Stock Stability\".";
+        projectilePrefabs = new GameObject[3];
+        spawnOffsetX = 10;
+        spawnOffsetY = 20;
+    }
+
+    public override void SpellUpdate()
+    {
+        if (projectileInstances.Count < 1) return;
+        if (cooldownCounter > 0)
+        {
+            cooldownCounter--;
+            return;
+        }
+        if (activateFlag)
+        {
+
+            // Reset the activate flag
+            activateFlag = false;
+            byte projectileIndex = (byte)(doesCrit ? 0 : (GameManager.Instance.seededRandom.Next(0, 100) < 50?1:2));
+
+            // Instantiate the projectile prefab at the player's position
+            // Assuming you have a reference to the player GameObject
+            if (owner != null && projectilePrefabs.Length > 1)
+            {
+                ProjectileManager.Instance.SpawnProjectile(projectileInstances[projectileIndex].GetComponent<BaseProjectile>(), owner.facingRight, new FixedVec2(Fixed.FromInt(spawnOffsetX), Fixed.FromInt(spawnOffsetY)));
+            }
+            cooldownCounter = cooldown;
+        }
+    }
+
+    public override void LoadSpell()
+    {
+        owner.stockStability += 15;
+        doesCrit = false;
+    }
+
+    public override void CheckCondition(PlayerController defender, ProcCondition targetProcCon)
+    {
+        switch(targetProcCon)
+        {
+            case ProcCondition.ActiveOnCast:
+                doesCrit = GameManager.Instance.seededRandom.Next(0, 100) < owner.stockStability;
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    
+}

436533705b3b5de823075aef18bd17d0d57e7013
Billy Guzik <guzikbilly@gmail.com>
2026-02-21 13:20:15 -0500

    began

---
+++ Spell Code/Assets/Scripts/Managers/ShopManagerv2.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ShopManagerv2 : MonoBehaviour
+{
+    //variables
+    public InputSnapshot[] inputSnapshots = new InputSnapshot[4];
+
+    private GameManager gM;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

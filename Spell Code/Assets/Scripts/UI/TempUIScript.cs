d55c1ba7dcac520c3d562feb020c63a64c6f0a36
Otiong <oscarloltiong@gmail.com>
2026-02-24 21:14:48 -0500

    new resource icons

--- Spell Code/Assets/Scripts/UI/TempUIScript.cs
+++ Spell Code/Assets/Scripts/UI/TempUIScript.cs
@@ -17,6 +17,7 @@ public class TempUIScript : MonoBehaviour
     public Sprite[] roundWinIcon;
     public Image[] flowStateVals;
     public TextMeshProUGUI[] stockStabilityVals;
+    public Image[] stockStabilityIcons;
     public Image[] demonAuraVals;
     public TextMeshProUGUI[] repsVals;
     public float flashAlpha = .5f;
@@ -88,13 +89,13 @@ public class TempUIScript : MonoBehaviour
             flowStateVals[i].fillAmount = (float)GameManager.Instance.players[i].flowState / PlayerController.maxFlowState;
 
             stockStabilityVals[i].enabled = true;
-            stockStabilityVals[i].text = "Crit: " + GameManager.Instance.players[i].stockStability;
+            stockStabilityVals[i].text = GameManager.Instance.players[i].stockStability.ToString();
             
             demonAuraVals[i].enabled = true;
             demonAuraVals[i].fillAmount = (float)GameManager.Instance.players[i].demonAura / PlayerController.maxDemonAura;
             
             repsVals[i].enabled = true;
-            repsVals[i].text = "REPS: " + GameManager.Instance.players[i].reps;
+            repsVals[i].text = GameManager.Instance.players[i].reps.ToString();
 
             //momentumVals[i].enabled = true;
             //momentumVals[i].fillAmount = (float)GameManager.Instance.players[i].momentum / 100;

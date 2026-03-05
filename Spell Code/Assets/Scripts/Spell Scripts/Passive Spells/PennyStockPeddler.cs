9f9c7d007a8cbf134cf2bc99cba915ba47c98864
FMPO <ple5024@rit.edu>
2026-03-05 12:20:01 -0500

    Update PennyStockPeddler.cs

--- Spell Code/Assets/Scripts/Spell Scripts/Passive Spells/PennyStockPeddler.cs
+++ Spell Code/Assets/Scripts/Spell Scripts/Passive Spells/PennyStockPeddler.cs
@@ -16,7 +16,7 @@ public class PennyStockPeddler : SpellData
         spellType = SpellType.Passive;
         procConditions = new ProcCondition[2] { ProcCondition.OnUpdate, ProcCondition.OnHitSpell };
         brands = new Brand[1] { Brand.BigStox };
-        description = "Your Stock Stability\" is capped at 15%. all excess Stock Stability is converted into bonus damage upon hitting an empowered \"Big Stox\" Spell";
+        description = "Your \"Stock Stability\" is capped at 15%. all excess Stock Stability is converted into bonus damage upon hitting an empowered \"Big Stox\" Spell";
     }
 
     public override void LoadSpell()

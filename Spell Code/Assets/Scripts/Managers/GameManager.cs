e466e45ae5ef418bb5c98061844a48459ec2881c
Reaper777 <jayeshsingh62@gmail.com>
2026-04-14 11:51:42 -0400

    reset ram instantly if number of spells acquired is >= 6

--- Spell Code/Assets/Scripts/Managers/GameManager.cs
+++ Spell Code/Assets/Scripts/Managers/GameManager.cs
@@ -1799,6 +1799,10 @@ public class GameManager : MonoBehaviour
                     }
                     else if (players[0].spellList.Count >= 6)
                     {
+                        for (int i = 0; i < playerCount; i++)
+                        {
+                            players[i].roundRam = 0; // reset round RAM
+                        }
                         playerWinText.enabled = false;
                         dataManager.totalRoundsPlayed += 1;
                         LoadRandomGameplayStage();

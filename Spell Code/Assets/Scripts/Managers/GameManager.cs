d27101d4130868f00d4baf5919d9ae710924ccec
Reaper777 <jayeshsingh62@gmail.com>
2026-04-14 14:35:10 -0400

    Revert "extra round win bug"
    This reverts commit bfacc19f98a896fbe3630c1d3923031658eb5ff2.

--- Spell Code/Assets/Scripts/Managers/GameManager.cs
+++ Spell Code/Assets/Scripts/Managers/GameManager.cs
@@ -2739,14 +2739,6 @@ public class GameManager : MonoBehaviour
                 SetStage(1);
             }
 
-            for (int i = 0; i < playerCount; i++)
-            {
-                if (players[i] != null)
-                {
-                    players[i].roundRam = 0;
-                }
-            }
-
             ResetPlayers();
             ProjectileManager.Instance.InitializeAllProjectiles();
             if (RollbackManager.Instance != null)
@@ -2807,14 +2799,6 @@ public class GameManager : MonoBehaviour
             if (RollbackManager.Instance != null)
             {
                 RollbackManager.Instance.ClearVars();
-            }
-
-            for (int i = 0; i < playerCount; i++)
-            {
-                if (players[i] != null)
-                {
-                    players[i].roundRam = 0;
-                }
             }
 
             InitializeOnlineShopSceneState();

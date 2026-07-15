ac00e852ea344906ecfeab7bf36613d2da885d58
Jayesh Singh <51245080+jayeshsingh62@users.noreply.github.com>
2026-07-15 11:16:20 -0400

    code mode menu won't open if the main menu screen is active

--- Spell Code/Assets/Scripts/UI/TempUIScript.cs
+++ Spell Code/Assets/Scripts/UI/TempUIScript.cs
@@ -350,7 +350,7 @@ public class TempUIScript : MonoBehaviour, ISelectHandler
         //     StartCoroutine(pause.SelectFirst(_tutorialPromptMenuFirst));
         //     TutorialPromptAnimation(0f, new Vector2 (-212f, 62f), new Vector2 (916f, 344f), new Vector2(1432f, 408f));
         // }
-        if (Input.GetKeyDown(KeyCode.Space) && !soloGamemodesMenuOpened && !multiplayerGamemodesMenuOpened && !codeModePromptMenuOpened && !pause.paused)
+        if (Input.GetKeyDown(KeyCode.Space) && !soloGamemodesMenuOpened && !multiplayerGamemodesMenuOpened && !codeModePromptMenuOpened && !pause.paused && !gameManager.MainMenuScreen.activeSelf)
         {
             OpenCodeModeMenuPrompt(true);
         }

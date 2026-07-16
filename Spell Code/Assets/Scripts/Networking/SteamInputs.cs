397bf46ededbb6289acc2686c48487912ef1e967
Billy Guzik <guzikbilly@gmail.com>
2026-07-16 12:32:15 -0400

    ughmhg

---
+++ Spell Code/Assets/Scripts/Networking/SteamInputs.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Steamworks;
+
+public class SteamInputs : MonoBehaviour
+{
+    private GameManager gM;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gM = GameManager.Instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

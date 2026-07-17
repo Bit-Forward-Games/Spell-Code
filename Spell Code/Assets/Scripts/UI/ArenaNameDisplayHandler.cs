7d6d29d5905d2aa98467f6dda0280655fdb46439
MaxWhite2024 <mjwhite2024@gmail.com>
2026-07-17 09:41:49 -0400

    Display Arena save

--- Spell Code/Assets/Scripts/UI/ArenaNameDisplayHandler.cs
+++ Spell Code/Assets/Scripts/UI/ArenaNameDisplayHandler.cs
@@ -18,37 +18,63 @@ public class ArenaNameDisplayHandler : MonoBehaviour
     /// <summary>
     /// Displays the current arena name at the bottom of the screen
     /// </summary>
-    public void DisplayArenaName(float _delay)
+    public Tween DisplayArenaName(float _duration)
     {
         //if arenaNameTextMesh does not exist, then return
-        if (_arenaNameTextMesh == null) return;
+        if (_arenaNameTextMesh == null) return null;
+
+        //if stage name does not exist OR is empty, then return
+        if (GameManager.Instance.stages[GameManager.Instance.currentStageIndex].stageName == null || GameManager.Instance.stages[GameManager.Instance.currentStageIndex].stageName == "") return null;
 
         //enable background
         _displayBackground.gameObject.SetActive(true);
 
         //change arenaNameTextMesh text show stage name
         _arenaNameTextMesh.text = GameManager.Instance.stages[GameManager.Instance.currentStageIndex].stageName;
+        Debug.Log("ArenaNameDisplay | Current arena name is: " + GameManager.Instance.stages[GameManager.Instance.currentStageIndex].stageName);
 
-        //wait for _delay seconds,...
-        DOVirtual.DelayedCall(_delay, () =>
-        {
-            //stop showing arena name
-            HideArenaName();
-        }).SetUpdate(false);
+        ////wait for _delay seconds,...
+        //DOVirtual.DelayedCall(_duration, () =>
+        //{
+        //    //stop showing arena name
+        //    HideArenaName();
+        //}).SetUpdate(false);
 
         //return
-        return;
+        return _displayBackground.gameObject.transform.DOPath
+        (
+            new Vector3[] { new Vector3(-53f, _displayBackground.gameObject.transform.position.y), new Vector3(53f, _displayBackground.gameObject.transform.position.y) },
+            _duration,
+            PathType.Linear,
+            PathMode.Sidescroller2D).SetEase(Ease.InOutElastic).OnComplete(() => HideArenaName()?.Invoke()
+        );
     }
+
+    //private TweenCallback DelayedHideArenaName(float _delay)
+    //{
+    //    return () =>
+    //    {
+    //        DOVirtual.DelayedCall(_delay,
+    //        () =>
+    //        {
+    //            //Hide name display
+    //            HideArenaName();
+    //        }
+    //        ).SetUpdate(true);
+    //    };
+    //}
 
     /// <summary>
     /// Stops showing the current arena name display
     /// </summary>
-    public void HideArenaName()
+    public TweenCallback HideArenaName()
     {
         //if arenaNameTextMesh does not exist, then return
-        if (_arenaNameTextMesh == null) return;
+        if (_arenaNameTextMesh == null) return null;
 
         //disable background
         _displayBackground.gameObject.SetActive(false);
+
+        return null;
     }
 }

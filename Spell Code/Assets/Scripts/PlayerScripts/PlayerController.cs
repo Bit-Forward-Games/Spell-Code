69f90a1657c65cb87c14ff407b2eb39aa7563360
FMPO <chrissante3.14@gmail.com>
2026-07-27 17:59:14 -0400

    RoA fix i think

--- Spell Code/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Spell Code/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -3362,6 +3362,7 @@ public class PlayerController : MonoBehaviour
                 VFX_Manager.Instance.PlayVisualEffect(VisualEffects.DEATH, position + FixedVec2.FromFloat(0f, 42f), pID);
             }    
 
+            ResetSpellResources();
             CheckAllSpellConditionsOfProcCon(this, ProcCondition.OnDeath);
 
             currentPlayerHealth = 0;

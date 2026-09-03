20febb752efea5eaa54000978feb74cd2cc514c6
FMPO <chrissante3.14@gmail.com>
2026-09-03 10:01:19 -0400

    codemehameha

---
+++ Spell Code/Assets/Scripts/Projectiles/DarkWeb/Codemehameha_prj.cs
@@ -0,0 +1,75 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using BestoNet.Types;
+
+using Fixed = BestoNet.Types.Fixed32;
+using FixedVec2 = BestoNet.Types.Vector2<BestoNet.Types.Fixed32>;
+
+public class Codemehameha_prj : BaseProjectile
+{
+    protected override void InitializeDefaults()
+    {
+        projName = "Chains Of Thanatos";
+        //hSpeed = 3f;
+        //vSpeed = 0f;
+        //lifeSpan = 35; // lasts for 300 logic frames
+        //deleteOnHit = true;
+        meleeProjectile = true;
+        fadeIn = true;
+        fadeOut = true;
+        animFrames = new AnimFrames(new List<int>(), new List<int>() { 4, 4, 4, 4, 4, 4, 4, 4}, true);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public override void SpawnProjectile(bool facingRight, FixedVec2 spawnOffset, string nameOverride = "", bool useAbsolutePosition = false)
+    {
+        base.SpawnProjectile(facingRight, spawnOffset, "", useAbsolutePosition);
+        hSpeed = Fixed.FromInt(0); // Set horizontal speed based on facing direction
+    }
+
+    public override void LoadProjectile()
+    {
+        projectileHitboxes = new HitboxGroup[2];
+        projectileHitboxes[1] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>
+            {
+                new HitboxData
+                {
+                    xOffset = 0,
+                    yOffset = 16*2,
+                    width = 42*2,
+                    height = 32*2,
+                    xKnockback = 2,
+                    yKnockback = 1,
+                    damage = 15,
+                    hitstun = 30,
+                    attackLvl = 2,
+                }
+            },
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+        projectileHitboxes[0] = new HitboxGroup
+        {
+            hitbox1 = new List<HitboxData>(),
+            hitbox2 = new List<HitboxData>(),
+            hitbox3 = new List<HitboxData>(),
+            hitbox4 = new List<HitboxData>()
+        };
+        frameData = new FrameData
+        {
+            startFrames = new List<int>
+            {
+                animFrames.frameLengths.Take(3).Sum()+1
+            },
+            endFrames = new List<int>
+            {
+                animFrames.frameLengths.Take(4).Sum()
+            }
+        };
+        base.LoadProjectile();
+    }
+}
